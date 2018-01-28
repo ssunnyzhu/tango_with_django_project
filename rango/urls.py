50ff9514aa859cc2072b6282b1beccf06eae84dd
Wenjia Zhu <2303595z@student.gla.ac.uk>
2018-01-28 00:02:20 +0000

    ch 8.3

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf.urls import include
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # here "$" is a blank in the website name
@@ -10,10 +11,10 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category, name='add_category'),
 #    url(r'^category/<category_name_slug>/add_page/$',
 #        views.add_page, name='add_page'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
-        views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+        views.add_page, name='add_page'),
 ]
 #This code imports the relevant Django machinery
 # for URL mappings and the views module from rango
