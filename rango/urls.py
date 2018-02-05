111dac1cb644ec30c2770dc2f8755f0267ae37d9
Wenjia Zhu <2303595z@student.gla.ac.uk>
2018-02-05 23:16:25 +0000

    ch9.6

--- rango/urls.py
+++ rango/urls.py
@@ -9,12 +9,17 @@ urlpatterns = [
     # here "$" is a blank in the website name
     url(r'^about', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
+
 #    url(r'^category/<category_name_slug>/add_page/$',
 #        views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category, name='show_category'),
+
     url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
         views.add_page, name='add_page'),
+    url(r'^register/$',
+        views.register,
+        name='register'), # New pattern!
 ]
 #This code imports the relevant Django machinery
 # for URL mappings and the views module from rango
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -98,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -106,6 +107,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 
 # Internationalization
