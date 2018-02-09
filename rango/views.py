34cf1e505706f8e9a880d733ae4c639fa88e4532
Wenjia Zhu <2303595z@student.gla.ac.uk>
2018-02-09 21:20:14 +0000

    ch9 finished

--- rango/views.py
+++ rango/views.py
@@ -215,7 +215,6 @@ def user_login(request):
         #  blank dictionary object...
         return render(request, 'rango/login.html', {})
 
-
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text!")
